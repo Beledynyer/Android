ec6283fb1288b2e97d7e5a6b2f5edad819d5caa1
Alvaro <alvarozb2003@gmail.com>
2024-07-26 21:21:22 +0200

    latest stuff

--- app/src/main/java/com/example/theagora/MainActivity.java
+++ app/src/main/java/com/example/theagora/MainActivity.java
@@ -8,9 +8,6 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-import org.conscrypt.Conscrypt;
-
-import java.security.Security;
 
 public class MainActivity extends AppCompatActivity {
 
