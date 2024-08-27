631e76cebcf067cd2444dd24303742e46282b164
Alvaro <alvarozb2003@gmail.com>
2024-08-27 21:04:34 +0200

    date format on RetrofitClientInstance

--- app/src/main/java/com/example/theagora/RetrofitClientInstance.java
+++ app/src/main/java/com/example/theagora/RetrofitClientInstance.java
@@ -1,5 +1,8 @@
 package com.example.theagora;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,9 +12,13 @@ public class RetrofitClientInstance {
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
+            Gson gson = new GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SS") // Matches the date format in your JSON
+                    .create();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
         return retrofit;
